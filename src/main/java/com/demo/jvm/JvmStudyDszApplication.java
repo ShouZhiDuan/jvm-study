89bff2ce2798cbe853f0182fcde42549f4a014d9
段守志 <shouzhi.duan@nvxclouds.com>
2021-01-08 16:25:24 +0800

    first commit

---
+++ src/main/java/com/demo/jvm/JvmStudyDszApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmStudyDszApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmStudyDszApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/jvm/JvmStudyDszApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmStudyDszApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
