001e274547a9bd31c0f40210dbcf21e4d7b8a814
段守志 <shouzhi.duan@nvxclouds.com>
2021-01-20 11:22:07 +0800

    Jvm指针压缩开启关闭。 Jvm指针压缩开启堆内存约束条件。

--- src/main/java/com/demo/jvm/hashcode_store/Worker.java
+++ src/main/java/com/demo/jvm/hashcode_store/Worker.java
@@ -10,24 +10,24 @@ import org.openjdk.jol.info.ClassLayout;
  * @Date: 2021/1/19 19:33
  * @Description:
  */
-//@Data
+@Data
 public class Worker {
-    //构造函数中作为初始入口，所以这种情况是不润许出现，否则出现StackOverflowError。
-    //private Worker worker1 = new Worker(1);
     private static Integer staticAge = 30;
     private final static Integer finalStaticAge = 31;
     private final  Integer finalAge = 32;
     private static Integer nullAge;
 
-    @Getter @Setter
-    private int age;
-
-    Worker(int age){
-       this.age = age;
-    }
-
     public static void main(String[] args) {
-        Worker worker = new Worker(2);
+        /**
+         * JVM Options：
+         *    -XX:-UseCompressedOops关闭压缩指针
+         *    -XX:+UseCompressedOops开启压缩指针
+         * 指针压缩：
+         *    4G < Mem(heap) < 32G 默认开启指针压缩。
+         *    Mem(heap) < 4G 关闭指针压缩。
+         *    Mem(heap) > 32G 指针压缩失效。
+         */
+        Worker worker = new Worker();
         String s = ClassLayout.parseInstance(worker).toPrintable();
         System.out.println(s);//输出当前对象各模块的大小。
         System.out.println(worker.hashCode());//当前类的hashcode值。
