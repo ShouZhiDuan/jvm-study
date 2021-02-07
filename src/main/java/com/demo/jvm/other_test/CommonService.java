ad7aa1b1072effd22d6435db9ae8faa5ab0015ed
段守志 <shouzhi.duan@nvxclouds.com>
2021-02-07 09:39:56 +0800

    fix code

---
+++ src/main/java/com/demo/jvm/other_test/CommonService.java
@@ -0,0 +1,11 @@
+package com.demo.jvm.other_test;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Date: 2021/2/7 9:38
+ * @Description: 实现一些公共的特性
+ */
+public abstract class CommonService implements TestService {
+
+
+}
