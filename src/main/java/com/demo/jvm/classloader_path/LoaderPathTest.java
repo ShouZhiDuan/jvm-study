09aae9c92a12772b85bb49ee56e510a81acadfe6
段守志 <shouzhi.duan@nvxclouds.com>
2021-02-19 16:08:01 +0800

    fix code

--- src/main/java/com/demo/jvm/classloader_path/LoaderPathTest.java
+++ src/main/java/com/demo/jvm/classloader_path/LoaderPathTest.java
@@ -1,8 +1,15 @@
 package com.demo.jvm.classloader_path;
 
-import java.io.IOException;
-import java.io.InputStream;
+import org.junit.Test;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @Auther: ShouZhi@Duan
@@ -12,6 +19,7 @@ import java.net.URL;
 public class LoaderPathTest {
 
     /**
+     *  ClassPath资源获取
      *  关于class以及class_loader的getResource的区别。
      *  class.getResource如果括号中是以"/"开头的则会在classPath路劲下寻找资源，如果不是"/"开头默认从当前class相同包路劲寻找资源。
      *  classLoader.getResource直接从classPath路劲下寻找。所以当前情况是不能以"/"开头的，否则会返回null。
@@ -30,6 +38,86 @@ public class LoaderPathTest {
         URL loaderResource = LoaderPathTest.class.getClassLoader().getResource("/");
         System.out.println(loaderResource);
     }
+
+
+    /**
+     *  磁盘资源
+     */
+    @Test
+    public void test() throws MalformedURLException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        File file = new File("D:\\test-class");
+        URL url = file.toURI().toURL();
+        URLClassLoader myClassLoader = new URLClassLoader(
+                new URL[] { url }, Thread.currentThread().getContextClassLoader());
+        Class<?> aClass = myClassLoader.loadClass("packTest.LoaderTest");
+        Object o = aClass.getConstructor().newInstance();
+        Method method = aClass.getMethod("test", null);
+        method.invoke(o,null);
+    }
+
+
+    /**
+     * jar包
+     */
+    public void test2(){
+        try {
+            File file = new File("rtest.txt");
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            String s = new String();
+            while ((s = in.readLine()) != null) {
+                //从rtest.txt中读取的url，根据url创建类装载器
+                URL url = new URL(s);
+                s = null;
+                URLClassLoader myClassLoader = new URLClassLoader(
+                        new URL[] { url }, Thread.currentThread().getContextClassLoader());
+                System.out.println(myClassLoader);
+                Class myClass = myClassLoader.loadClass("uRLClassLoaderTest1.TestAction");
+                Object action = myClass.newInstance();
+                //String str = action.action();
+                //System.out.println(str);
+
+                //根据url1创建类装载器
+                URL url1 = new URL("file:d:/test.jar");
+                URLClassLoader myClassLoader1 = new URLClassLoader(
+                        new URL[] { url1 }, Thread.currentThread().getContextClassLoader());
+                Class myClass1 = myClassLoader1.loadClass("uRLClassLoaderTest1.TestAction");
+                Object action1 =  myClass1.newInstance();
+                //String str1 = action1.action();
+                //System.out.println(str1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * test
+     */
+    @Test
+    public void test3(){
+        //System.out.println(System.getProperty("java.class.path"));
+        System.out.println(System.getProperty("sun.cpu.isalist"));
+        Map<String, String> getenv = System.getenv();
+        Properties properties = System.getProperties();
+        System.out.println();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/demo/jvm/my_classloader/MyUrlClassLoader.java
+++ src/main/java/com/demo/jvm/my_classloader/MyUrlClassLoader.java
@@ -1,12 +1,14 @@
 package com.demo.jvm.my_classloader;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 /**
  * @Auther: ShouZhi@Duan
  * @Date: 2021/2/19 10:45
- * @Description:
+ * @Description: https://www.iteye.com/topic/136427
  */
 public class MyUrlClassLoader extends URLClassLoader {
 
@@ -20,10 +22,29 @@ public class MyUrlClassLoader extends URLClassLoader {
         super(urls, parent);
     }
 
+    public Class load(String name) throws ClassNotFoundException {
+        return load(name, false );
+    }
+
+    public Class load(String name, boolean resolve) throws ClassNotFoundException {
+        if ( null != super .findLoadedClass(name)) return reload(name, resolve);
+        Class clazz = super .findClass(name);
+        if (resolve) super .resolveClass(clazz);
+        return clazz;
+    }
+
+    public Class reload(String name, boolean resolve) throws ClassNotFoundException {
+        return new MyUrlClassLoader(super.getURLs(), super.getParent()).load(name, resolve);
+    }
 
 
 
-
-
-
+    public static void main(String[] args) throws MalformedURLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        MyUrlClassLoader classLoader = new MyUrlClassLoader(new URL[]{new URL("file:/C:/Users/dev/Desktop/test/")});
+        Class<?> aClass = classLoader.load("test.LoaderTest");
+        Object instance = aClass.newInstance();
+        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{new URL("file:/C:/Users/dev/Desktop/test/")});
+        new File("").toURI().toURL();
+        System.out.println();
+    }
 }
